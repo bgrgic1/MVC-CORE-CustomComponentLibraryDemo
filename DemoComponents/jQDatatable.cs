4399105adc3afa7a6fb1ef01914695fa1754762a
Borislav Grgić <borislav.grgic@shipshape.hr>
2020-12-14 13:59:44 +0100

    Add project files.

---
+++ DemoComponents/jQDatatable.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoComponents
+{
+    [ViewComponent(Name = "jQDatatable")]
+    public class jQDatatableViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(string model)
+        {
+            return View(model);
+        }
+
+    }
+}
